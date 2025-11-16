335a84f2c2a4d07747034982f666fe3ae7c15b02
DraconDev <dracsharp@gmail.com>
2025-11-16 01:50:18 +0000

    refactor(subscription): Update package name Changed package name from  server  to  subscription  for better organization.

---
+++ internal/server/index.go
@@ -0,0 +1,5 @@
+// index.go - Main package file for the server module
+// This file establishes the server package in the root directory
+// All files in subdirectories also declare "package server"
+// making them part of the same logical package
+package server
