fbeddf35d6bc9afeb9b61a0e0f7de6ca7d7d7d38
DraconDev <dracsharp@gmail.com>
2025-11-16 01:50:47 +0000

    feat(server): Export HTTPServer and NewHTTPServer

--- internal/server/index.go
+++ internal/server/index.go
@@ -3,3 +3,22 @@
 // All files in subdirectories also declare "package server"
 // making them part of the same logical package
 package server
+
+// Export main types and functions for external use
+// These are defined in subdirectories but accessible throughout the server package
+
+// HTTPServer is the main HTTP server struct
+type HTTPServer struct {
+	db           database.RepositoryInterface
+	stripeSecret string
+}
+
+// NewHTTPServer creates a new HTTP server instance
+func NewHTTPServer(db database.RepositoryInterface, stripeSecret string) *HTTPServer {
+	stripe.Key = stripeSecret
+
+	return &HTTPServer{
+		db:           db,
+		stripeSecret: stripeSecret,
+	}
+}
